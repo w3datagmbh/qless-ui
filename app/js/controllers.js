24e84c4ce728f55c86278af5d43d9ce828ff4ecc
Simon Wörner <git@simon-woerner.de>
2016-03-11 11:14:34 +0100

    clean up, refresh, ungrouped queues

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,20 +4,35 @@
 
 var qlessuiControllers = angular.module('qlessuiControllers', ['angularBootstrapNavTree']);
 
-qlessuiControllers.controller('QueuesListCtrl', ['$scope', 'Groups', 'Group', 'Queues',
-  function($scope, Groups, Group, Queues) {
+qlessuiControllers.controller('QueuesListCtrl', ['$scope', 'Groups', 'Queues',
+  function($scope, Groups, Queues) {
     $scope.groups = Groups.nav();
-    $scope.queues = Queues.counts();
+    $scope.queues = Queues.query();
     $scope.groups_tree = {};
+    var regexStr = '.*';
 
     $scope.on_select_group = function(branch) {
-        $scope.queues = branch.data ? Group.get({regex_str: branch.data}) : null;
+        regexStr = branch.data ? branch.data : null;
+        $scope.on_refresh();
+    }
+    $scope.on_refresh = function() {
+        $scope.queues = regexStr ? Groups.get({regexStr: regexStr}) : null;
+    }
+    $scope.on_queue_play = function(queueName) {
+        console.log(queueName);
+        // TODO API CALL
+        $scope.on_refresh();
+    }
+    $scope.on_queue_pause = function(queueName) {
+        console.log(queueName);
+        // TODO API CALL
+        $scope.on_refresh();
     }
   }
 ]);
 
-qlessuiControllers.controller('QueuesGetCtrl', ['$scope', '$routeParams', 'Queue',
-  function($scope, $routeParams, Queue) {
-    $scope.queue = Queue.get({queueName: $routeParams.queueName});
+qlessuiControllers.controller('QueuesGetCtrl', ['$scope', '$routeParams', 'Queues',
+  function($scope, $routeParams, Queues) {
+        $scope.queue = Queues.get({queueName: $routeParams.queueName});
   }
 ]);
--- app/js/services.js
+++ app/js/services.js
@@ -8,35 +8,22 @@ var qlessPyUi = '/api';
 qlessuiServices.factory('Config', ['$resource',
   function($resource){
     return $resource(qlessPyUi + '/config', {}, {
-      get: {method:'GET'}
+      get: { method:'GET' }
     });
   }]);
 
 qlessuiServices.factory('Groups', ['$resource',
   function($resource){
-    return $resource(qlessPyUi + '/groups', {}, {
-      nav: {method:'GET', isArray:true}
+    return $resource(qlessPyUi + '/groups/:regexStr', { regexStr: null }, {
+        nav: { method:'GET', isArray:true },
+        get: { method:'GET', isArray:true, params: { regexStr: '.*' } }
     });
   }]);
-
-qlessuiServices.factory('Group', ['$resource',
-  function($resource){
-    return $resource(qlessPyUi + '/groups/:regex_str', {}, {
-      get: {method:'GET', isArray:true}
-    });
-  }]);
-
 
 qlessuiServices.factory('Queues', ['$resource',
   function($resource){
-    return $resource(qlessPyUi + '/queues', {}, {
-      counts: {method:'GET', isArray:true}
-    });
-  }]);
-
-qlessuiServices.factory('Queue', ['$resource',
-  function($resource){
-    return $resource(qlessPyUi + '/queues/:queueName', {}, {
-      get: {method:'GET'}
+    return $resource(qlessPyUi + '/queues/:queueName', { queueName: null }, {
+        query: { method:'GET', isArray:true },
+        get: { method:'GET', params: { queueName: '' } }
     });
   }]);
